edd5105258201e30ff898cc6ed02bc7dd1a3023b
Eric Tanner <ertanner@gmail.com>
2018-02-08 14:18:53 -0800

    Added Signup.vue

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -53,21 +53,55 @@
 </template>
 
 <script>
-export default {
-  name: 'SignUp',
-  data () {
-    return {
-      msg: '',
-      // We need to initialize the component with any
-      // properties that will be used in it
-      credentials: {
-        accountname: '',
-        username: '',
-        password: ''
+  import axios from 'axios'
+  export default {
+    name: 'SignUp',
+    data () {
+      return {
+        msg: '',
+        // We need to initialize the component with any
+        // properties that will be used in it
+        credentials: {
+          accountname: '',
+          username: '',
+          password: ''
+        },
+        error: ''
+      }
+    },
+    methods: {
+      submit () {
+        var credentials = {
+          username: this.credentials.username,
+          password: this.credentials.password
+        }
+        console.log(credentials)
+        this.fetchData()
+        // We need to pass the component's this context
+        // to properly make use of http in the auth service
+        // auth.login(this, credentials, 'secretquote')
       },
-      error: ''
+      fetchData () {
+        var str = ''
+        console.log('late')
+        str = 'http://localhost:9080/getUser/'
+
+        console.log('URL: ' + str)
+
+        axios.post(str,
+          this.credentials,
+          {
+            headers: {
+              'Content-type': 'application/x-www-form-urlencoded'
+            }
+          }).then(response => {
+            this.trips = response.data
+          })
+          .catch(e => {
+            this.errors.push(e)
+          })
+      }
     }
-  }
 }
 </script>
 
